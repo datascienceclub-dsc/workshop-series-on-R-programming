aea5c5133d9845e9c5661014bc5c52512accfe10
Rakesh Poduval <poduval.rakesh@icloud.com>
2023-09-16 07:51:11 +0530

    aadded code for generalize mle function

--- 6-Functional programming/MLE simulations.R
+++ 6-Functional programming/MLE simulations.R
@@ -22,7 +22,7 @@ fn <- function(f, ...) f(...)
 fn(optimise, 
    log_likelihood, 
    interval = c(0, 100), 
-   x = rpois(n = 50, lambda = 0.3), 
+   x = fn(rpois, n = 50, lambda = 0.3), 
    maximum = TRUE)$maximum
 
 # Single iterations, Varying sample size ====
@@ -115,28 +115,83 @@ library(purrr)
 
 #' perf
 #' #' @description performance measurement
+#' @examples
+#' perf(runif(100, 0, 1), 0.4)
+#' perf(rpois(100, 0.4), 0.4)
 #' 
 perf <- function(x, target = mean(x)) {
+  
   data.frame(bias = mean(x - target),
              mabs = mean(abs(x - target)),
              mse = mean((x - target) ^ 2),
-             rmse = sqrt(mean((x - target) ^ 2)))
+             rmse = sqrt(mean((x - target) ^ 2)),
+             target = target)
   }
 
 #' mlepois
 #' @description: 
 #' 
-mlepois <- function(n_size, n_iter = 10) {
+mlepois <- function(n_size, n_iter = 10, lambda = 0.3) {
   ll <- map_dbl(1:n_iter, ~ fn(optimise, log_likelihood, 
                                interval = c(0, 100), 
-                               x = fn(rpois, n = n_size, lambda = 0.3), 
+                               x = fn(rpois, n = n_size, lambda = lambda), 
                                maximum = TRUE)$maximum)
-  data.frame(sample = paste0("n=", n_size), perf(ll, target = 0.3))
+  data.frame(sample = paste0("n=", n_size), perf(ll, target = lambda))
 }
 
-mlepois(50, n_iter = 100)
-do.call(rbind, lapply(c(50, 100, 200), mlepois, n_iter = 100))
+mlepois(n_size = 50, n_iter = 100, lambda = 0.3)
+mlepois(n_size = 50, n_iter = 100, lambda = 0.4)
+
+sample_size <- c(50, 100, 200)
+do.call(rbind, lapply(sample_size, mlepois, n_iter = 100))
 
 library(dplyr)
-map(seq(50, 500, 50), mlepois, n_iter = 1000) %>% bind_rows
+map(sample_size, mlepois, n_iter = 1000, lambda = 0.3) %>% bind_rows
 
+## Varying lambda ====
+params <- expand.grid(n_size = sample_size, lambda = c(0.2, 0.3, 0.4))
+params
+pmap(params, mlepois, n_iter = 100) %>% bind_rows()
+
+# ------------------------------------------------------------------------------
+
+# Generalizing MLE function ====
+#' add possibility for making flexible the function to generate X 
+#' and it's optimization function
+
+#' mle
+#' @description: finding...
+#' 
+#' @param n_size sample size
+#' @param n_iter number of iterations
+#' @param fn_rand function to generate random numbers
+#' @param fn_optim function to optimize
+#' @param target target value to compare
+#' @param ... additional parameters to the functions
+#' 
+mle <- function(n_size, n_iter, fn_rand, fn_optim, target, ...) {
+  
+  ll <- map_dbl(1:n_iter, ~ fn(optimise, fn_optim, 
+                               interval = c(0, 100), 
+                               x = fn(fn_rand, n = n_size, ...), 
+                               maximum = TRUE)$maximum)
+  data.frame(sample = paste0("n=", n_size), perf(ll, target = target))
+}
+
+mle(n_size = 50, n_iter = 100, fn_rand = rpois, fn_optim = log_likelihood,
+    target = 0.3, lambda = 0.3)
+
+sample_size <- c(50, 100, 200)
+
+library(dplyr)
+map(sample_size, mle, n_iter = 100, 
+    fn_rand = rpois, fn_optim = log_likelihood,
+    target = 0.3, lambda = 0.3) %>%
+  bind_rows
+
+params <- expand.grid(n_size = sample_size, 
+                      target = c(0.2, 0.3, 0.4),
+                      lambda = 0.3,
+                      n_iter = 100)
+params
+pmap(params, mle, fn_rand = rpois, fn_optim = log_likelihood) %>% bind_rows()
